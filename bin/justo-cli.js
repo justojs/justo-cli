12592d5efe2c7d713fb8b871e44c4db2241b6e2c
Raúl G. González <raulggonzalez@justojs.org>
2016-04-05 12:04:33 +0200

    Fix #37

--- bin/justo-cli.js
+++ bin/justo-cli.js
@@ -36,6 +36,11 @@ opts = yargs
     type: "boolean",
     default: false
   })
+  .option("issue", {
+    describe: "Show how to notify an issue.",
+    type: "boolean",
+    default: false
+  })
   .option("o", {
     alias: "only",
     describe: "Only run suites and tests fixed as only.",
@@ -56,11 +61,13 @@ opts = yargs
   })
   .version("v", "Show CLI version.", pkg.version)
   .alias("v", "version")
-  .epilogue(`Bugs: github.com/justojs/justo-issues\n      issues@justojs.org\n\nProudly made with ♥ in Valencia, Spain, EU.\nCopyright (c) ${pkg.years} ${pkg.author.name}.\nIn memory of Justo González Mallols.`)
+  .epilogue(`Proudly made with ♥ in Valencia, Spain, EU.\nCopyright (c) ${pkg.years} ${pkg.author.name}.\nIn memory of Justo González Mallols.`)
   .argv;
 
 //(3) run
-if (opts.generate) {
+if (opts.issue) {
+  console.log("Web: https://github.com/justojs/justo-issues\nEmail: issues@justojs.org");
+} else if (opts.generate) {
   Cli.generate(opts.generate.toLowerCase(), opts._, {parse: opts.parse, mute: opts.mute});
 } else if (opts.list) {
   Cli.listCatalogedTasks("./Justo.json");
